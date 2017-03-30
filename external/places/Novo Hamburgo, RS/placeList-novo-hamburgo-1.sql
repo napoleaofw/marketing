294209f4b90181551d1a8fd7c893d1b5a7947128
Napoleão Fusiger Wingert <napoleao@kunden.local>
2017-03-30 08:40:32 -0300

    records++

--- external/places/Novo Hamburgo, RS/placeList-novo-hamburgo-1.sql
+++ external/places/Novo Hamburgo, RS/placeList-novo-hamburgo-1.sql
@@ -7983,7 +7983,7 @@ BEGIN
         END IF;
         CLOSE cursorTagMax;
         SET v_tag_id_max = v_tag_id_max + 1;
-        SET v_tag_name = 'McDonalds,Mc Entrega, Restaurante,FastFood,Mc Donalds,Fast Food  McDonald's';
+        SET v_tag_name = 'McDonalds,Mc Entrega, Restaurante,FastFood,Mc Donalds,Fast Food  McDonald\'s';
         SET v_cursor_not_found = FALSE;
         OPEN cursorTag;
         FETCH cursorTag INTO v_tag_id_exists;
